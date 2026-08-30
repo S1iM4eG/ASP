d4f1968dac3d84366b65c1c266e04161992bed51
S1iM <S1iM@HOME-PC>
2026-08-30 21:28:54 +0400

    HW-1

---
+++ Blazor/Blazor/Components/Pages/Fibonacci.razor.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Fibonacci
+    {
+        int count;
+        long[] res;
+
+        /*void Сalculation()
+        {
+            if (count <= 0)
+            {
+                res = [0];
+                return;
+            }
+
+            res = new long[count];
+            res[0] = 0;
+
+            if (count == 1) return;
+
+            res[1] = 1;
+            for (int i = 2; i < count; i++)
+            {
+                res[i] = res[i - 1] + res[i - 2];
+            }
+        }*/
+    }
+}
---
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Power
+    {
+        int n = 0;
+        int p = 0;
+        Double res = 1;
+        void Multiply()
+        {
+            res = 1;
+            if (n == 0 && p == 0) res = 0;
+            else if (n != 0 && p == 0) res = 1;
+            else if (n == 0 && p < 0) res = 0;
+            else if (p > 0)
+            {
+                for (int i = 0; i < p; i++)
+                {
+                    res *= n;
+                }
+            }
+            else if (p < 0)
+            {
+                for (int i = 0; i > p; i--)
+                {
+                    res *= n;
+                }
+                res = 1 / res;
+            }
+        }
+    }
+}
