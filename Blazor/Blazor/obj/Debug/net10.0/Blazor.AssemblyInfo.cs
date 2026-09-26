17fc8c500d1abceacd14c308605fffbfbf86cf94
S1iM <S1iM@HOME-PC>
2026-09-26 21:01:48 +0400

    Lesson 31 - Students Search DONE

--- Blazor/Blazor/obj/Debug/net10.0/Blazor.AssemblyInfo.cs
+++ Blazor/Blazor/obj/Debug/net10.0/Blazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa852b6e3cb0205d088aed2bc24c259e0a5dd3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2be219abecde97498a53ff202043134e7d11adcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Blazor/Movies/obj/Debug/net10.0/Movies.AssemblyInfo.cs
+++ Blazor/Movies/obj/Debug/net10.0/Movies.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa852b6e3cb0205d088aed2bc24c259e0a5dd3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2be219abecde97498a53ff202043134e7d11adcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
